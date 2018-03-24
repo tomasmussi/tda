6b3bbdce6afa55d595966f45c9ffb096a6de0d0d
Tomas Mussi <tomasmussi@gmail.com>
2018-03-24 11:32:09 -0300

    Agrego comentarios y dejo todo listo como para hacer el algoritmo de GS

--- tp1/gale_shapley.py
+++ tp1/gale_shapley.py
@@ -5,16 +5,25 @@ N_PLAYERS = 200
 N_TEAMS = 20
 DIR_NAME = "gs-instance/"
 
+"""
+Escribe a un archivo las preferencias generadas para un team/player
+"""
 def write_file(filename, preferences):
 	with open(filename, 'wb') as f:
 		for number in preferences:
 			f.write(str(number) + '\n')
 
+"""
+Lee de un archivo las preferencias generadas
+"""
 def get_numbers_from_file(filename):
 	with open(filename, 'r') as f:
 		content = f.readlines()
 	return [int(x) for x in content]
 
+"""
+Genera una instancia de preferencias de players y teams
+"""
 def generate_instance():
 	if (not os.path.isdir(DIR_NAME)):
 		os.makedirs(DIR_NAME)
@@ -30,19 +39,34 @@ def generate_instance():
 			shuffle(team_preference)
 			write_file(DIR_NAME + "team_" + str(team) + ".txt", team_preference)
 
+"""
+Obtiene en forma de matriz las preferencias de todos los players y teams
+En el indice i, esta la preferencia del player/team i
+"""
+def get_preferences():
+	team_prefs = []
+	for i in xrange(N_TEAMS):
+		t_pref = get_numbers_from_file(DIR_NAME + "team_" + str(i) + ".txt")
+		team_prefs.append(t_pref)
+	player_prefs = []
+	for i in xrange(N_PLAYERS):
+		p_pref = get_numbers_from_file(DIR_NAME + "player_" + str(i) + ".txt")
+		player_prefs.append(p_pref)
+	return team_prefs, player_prefs
 
-def gale_shapley():
+"""
+Algoritmo de Gale Shapley para encontrar un matching estable de equipos
+"""
+def gale_shapley(team_prefs, player_prefs):
+	print team_prefs
+	print player_prefs
 	raise NotImplementedError()
 
 
 def main():
 	generate_instance()
-	player198 = get_numbers_from_file(DIR_NAME + "player_198.txt")
-	team3 = get_numbers_from_file(DIR_NAME + "team_3.txt")
-	team7 = get_numbers_from_file(DIR_NAME + "team_7.txt")
-	print team3
-	print team7
-	print player198
+	team_prefs, player_prefs = get_preferences()
+	gale_shapley(team_prefs, player_prefs)
 
 
 
