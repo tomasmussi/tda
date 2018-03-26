19b2496319554984d22d5e6a41609c66795a4039
tomas <tomasmussi@gmail.com>
2018-03-26 18:09:56 -0300

    Agrego comentarios sobre las variables del algoritmo de gale shapley y una funcion para verificar sm

--- tp1/gale_shapley.py
+++ tp1/gale_shapley.py
@@ -56,12 +56,36 @@ def get_preferences():
 
 """
 Algoritmo de Gale Shapley para encontrar un matching estable de equipos
+
+matches : tuplas que van conformando el matching estable. Cada tupla es: (team, player)
+
+vacancies : vacantes que tiene cada team. En la posicion sub i, esta la cantida de vacantes que tiene el equipo sub i
+Hecho asi para poder acceder en O(1) a la cantidad de vacantes y decidir si hay que sacar o meter al equipo de la lista de
+equipos no llenos
+
+team_prefs_index : team_prefs_index[i] representa el indice a buscar dentro de la lista de preferencias del Team i,
+es decir, quien sera el proximo jugador al cual hacerle una propuesta para ingregar al equiopo.
+
+teams_not_full: es el listado de los N_TEAMS teams a los cuales hay que buscar vacantes.
+Una vez que tiene 10 jugadores, se lo elimina de esta lista.
+Si un jugador decide irse a otro equipo y no esta en esta lista, vuelve a ingresar, dado que tiene que cubrir una vacante
+
+player_team : el player_team[i] representa el Team en el cual esta el Player sub i.
+Hecho asi para poder acceder al equipo en O(1)
+
+matrix_preference: es una matriz de N_PLAYERS filas por N_TEAMS columnas, donde se lleva registro de cual es el valor de
+preferencia que tiene cada jugador para cada equipo.
+matrix_preference[i] es la fila de preferencias del Player i, por lo que matrix_preference[i][j] es el valor que tiene
+Player i para Team j. A menor valor, mayor preferencia por dicho equipo.
+Se utiliza para realizar comparaciones en tiempo constante de si un jugador prefiere un Team u otro para tomar la decision
+de quedarse o irse de un Team
+
 """
 def gale_shapley(team_prefs, player_prefs):
 
 	matches = {}
 	vacancies = [N_PLAYERS / N_TEAMS for i in range(N_TEAMS)]
-	team_prefs_index = [0 for i in range(N_TEAMS)]
+	team_prefs_index = [0 for i in range(N_PLAYERS)]
 	teams_not_full = [i for i in range(N_TEAMS)]
 	player_team = [-1 for i in range(N_PLAYERS)]
 	matrix_preference = [{} for i in range(N_PLAYERS)]
@@ -98,8 +122,25 @@ def gale_shapley(team_prefs, player_prefs):
 
 	return matches
 
+"""
+Para verificar que el resultado obtenido es valido se deben verificar:
+1) Hay 20 equipos con 10 personas para cada equipo
+2) Para cada combinacion de (team, player) no pueden verificarse ambas condiciones
+OtroTeam tiene preferencia por un Player de Team
+Player tiene preferencia por OtroTeam antes que por Team
+"""
 def is_stable_matching(sm, team_prefs, player_prefs):
-	return False
+	matrix = {}
+	for k in sm.keys():
+		if k[0] in matrix:
+			matrix[k[0]].append(k[1])
+		else:
+			matrix[k[0]] = [k[1]]
+	for k in matrix:
+		if (len(matrix[k]) != (N_PLAYERS / N_TEAMS)):
+			return False
+	# TODO(tmussi) Falta verificar que es matching estable.
+	return True
 
 
 def main():
