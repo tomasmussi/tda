cb5a1621a0bfd7f26eb404eee74cd827322c6ef2
Santiago Alcala <santiago.alcala@wolox.com.ar>
2018-04-29 20:00:28 -0300

    Mapa actualziado, primer aproximacion del punto 1 2

--- tp2/spy_spy.py
+++ tp2/spy_spy.py
@@ -1,36 +1,39 @@
 import sys
 from grafo import Grafo
 
-def llenarGrafo(grafo):
-	with open('mapa.coords') as openfileobject:
-			for line in openfileobject:
-				vertices = line.split(' - ')
-				v1 = vertices[0].rstrip()
-				v2 = vertices[1].rstrip()
-				grafo.agregarVertice(v1)
-				grafo.agregarVertice(v2)
-				grafo.agregarArista(v1, v2)
-	print grafo.vertices
+
+def llenar_grafo(grafo):
+    with open('mapa.coords') as openfileobject:
+        for line in openfileobject:
+            vertices = line.split(' - ')
+            v1 = vertices[0].rstrip()
+            v2 = vertices[1].rstrip()
+            grafo.agregarVertice(v1)
+            grafo.agregarVertice(v2)
+            grafo.agregarArista(v1, v2)
 
 def main():
-	if (len(sys.argv) != 7):
-		raise Exception('Debe ingresar las coordenadas x,y de los espias y del aeropuerto')
-	spy1 = (int(sys.argv[1]), int(sys.argv[2]))
-	spy2 = (int(sys.argv[3]), int(sys.argv[4]))
-	aeropuerto = (int(sys.argv[5]), int(sys.argv[6]))
+    if (len(sys.argv) != 7):
+        raise Exception(
+            'Debe ingresar las coordenadas x,y de los espias y del aeropuerto')
+    spy1 = ' '.join([sys.argv[1], sys.argv[2]])
+    spy2 = ' '.join([sys.argv[3], sys.argv[4]])
+    aeropuerto = ' '.join([sys.argv[5], sys.argv[6]])
+    
+    print "El espia 1 esta en posicion " + spy1
+    print "El espia 2 esta en posicion " + spy2
+    print "El aeropuerto esta en posicion " + aeropuerto
 
-	print "El espia 1 esta en posicion " + str(spy1)
-	print "El espia 2 esta en posicion " + str(spy2)
-	print "El aeropuerto esta en posicion " + str(aeropuerto)
+    grafo = Grafo()
+
+    llenar_grafo(grafo)
+    
+    print "espia 1 hasta aeropuerto " + str(grafo.distanciaMinima(spy1, aeropuerto))
+    print "espia 2 hasta aeropuerto " + str(grafo.distanciaMinima(spy2, aeropuerto))
 
 
-
-	grafo = Grafo()
-	llenarGrafo(grafo)
-
-
-	# TODO: Resolver...
+# TODO: Resolver...
 
 
 if __name__ == '__main__':
-	main()
+    main()
