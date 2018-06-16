8bb2dfce00a03d90edc70e59d05f0e4c74d1512b
Santiago Alcala <santyalcala@users.noreply.github.com>
2018-06-16 17:47:31 -0300

    Update prueba_flujo.py

--- tp3/prueba_flujo.py
+++ tp3/prueba_flujo.py
@@ -18,9 +18,12 @@ grafo.agregarArista(3,4,2)
 # buscar maximo
 for i in range(0,1):
 
+    # Hago una copia del grafo original para despues comparar.
     grafo_viejo = copy.deepcopy(grafo)
+    # Calculo ford fulkerson
     print "Flujo maximo" + str(grafo.ford_fulkerson(1,4))
 
+    # Paso los grafos a una forma "plana", ej: grafo[1][2] = 3 <=> grafo[1,2] = 3
     grafo_viejo_plano = {}
     grafo_residual_plano = {}
     for key in grafo_viejo.vertices:
@@ -30,14 +33,12 @@ for i in range(0,1):
     for key in grafo.vertices:
         for subkey in grafo.vertices[key]:
             grafo_residual_plano[key, subkey] = grafo.vertices[key][subkey]
-
+    
+    # Calculo diferencia entre flujos
     diferencia_flujos = {x: grafo_viejo_plano[x] - grafo_residual_plano[x] for x in grafo_viejo_plano if x in grafo_residual_plano}
     max_diferencia = max([(value, key) for key, value in diferencia_flujos.items()])[1]
     print "Custodiar el" + str(max_diferencia)
 
+    # Calculo otra copia para correr de nuevo el algoritmo.
     grafo = copy.deepcopy(grafo_viejo)
     grafo[max_diferencia(0)].pop(max_diferencia[1])
-
-
-
-
