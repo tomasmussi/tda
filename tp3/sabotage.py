94d890fadebc27af9d3cd7c81aa77e97c44911e5
Santiago Alcala <santiago.alcala@wolox.com.ar>
2018-06-17 18:33:50 -0300

    Agregado el segundo metodo

--- tp3/sabotage.py
+++ tp3/sabotage.py
@@ -5,6 +5,7 @@ import copy
 from grafo import Grafo
 import operator
 
+MAX_PROTECTED = 2
 
 def read_net(file_net):
 	net = Grafo()
@@ -25,6 +26,22 @@ def read_net(file_net):
 		net.agregarArista(t[0], t[1], edges[t])
 	return net
 
+def protection_method_two(net):
+	# Fuerza bruta, me fijo cuales dos aristas al sacarlas hacen que le grafo le baje el flujo la mayor cantidad
+	original_net_plain = net.convertir_plano()
+	for _ in range(0, MAX_PROTECTED):
+		flujos = {}
+		for key in original_net_plain:
+			original_net = copy.deepcopy(net)
+			original_net.borrarArista(list(key)[0], list(key)[1])
+			flujos[key] = original_net.ford_fulkerson(0, 1)
+
+		flujos = sorted(flujos.items(), key=operator.itemgetter(1))
+		most_important_edge, most_important_edge_maximum_flow = flujos[0][0], flujos[0][1]
+
+		print 'El flujo se vuelve minimo al sacar la arista: ' + str(most_important_edge) + ' dejando un flujo de ' + str(most_important_edge_maximum_flow)
+		net.borrarArista(most_important_edge[0], most_important_edge[1])
+
 def protection_method_one(net):
 	original_net = copy.deepcopy(net)
 	# Calculo ford fulkerson
@@ -44,15 +61,20 @@ def protection_method_one(net):
 	second_edge, second_edge_flux = list(delta_flux[-2][0]), delta_flux[-2][1]
 
 	print "Custodiar el: " + str(first_edge) + ' que lleva un flujo de ' + str(first_edge_flux) + \
-			' y el: ' + str(second_edge) + ' que lleva un flujo de ' +  str(second_edge_flux)
+		' y el: ' + str(second_edge) + ' que lleva un flujo de ' +  str(second_edge_flux)
 
 	net_without_maximum_flow_edge = copy.deepcopy(original_net)
 	net_without_second_maximum_flow_edge = copy.deepcopy(original_net)
+	net_without_first_and_second_maximum_flow_edge = copy.deepcopy(original_net)
 	net_without_maximum_flow_edge.borrarArista(first_edge[0], first_edge[1])
 	net_without_second_maximum_flow_edge.borrarArista(second_edge[0], second_edge[1])
+	net_without_first_and_second_maximum_flow_edge.borrarArista(first_edge[0], first_edge[1])
+	net_without_first_and_second_maximum_flow_edge.borrarArista(second_edge[0], second_edge[1])
+	
+	print "Luego el flujo sin la primer arista es: " + str(net_without_maximum_flow_edge.ford_fulkerson(0, 1)) + \
+		" y el flujo sin la segunda arista es: " + str(net_without_second_maximum_flow_edge.ford_fulkerson(0, 1))
 
-	print "Luego el flujo sin la primer arista es: " + str(net_without_maximum_flow_edge.ford_fulkerson(0, 1))
-	print " y finalmente el flujo sin la segunda arista es: " + str(net_without_second_maximum_flow_edge.ford_fulkerson(0, 1))
+	print "Finalmente el flujo sin ninguna de ambas aristas es de: " + str(net_without_first_and_second_maximum_flow_edge.ford_fulkerson(0, 1))
 
 def main():
 	if (len(sys.argv) == 1):
@@ -77,7 +99,11 @@ def main():
 
 	net = read_net(file_net)
 	# Hago una copia porque adentro la rompo toda
+	print "Metodo averiguando las dos que llevan mas flujo"
 	protection_method_one(copy.deepcopy(net))
+
+	print '\nMetodo por fuerza bruta, sacando una arista a la vez y viendo en que caso el flujo es minimo'
+	protection_method_two(copy.deepcopy(net))
 
 
 
